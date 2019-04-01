dce4769074853fde2b8295fe67cdfa0463be1bf4
Enmanuel Aponte <enmanuelaponte1@gmail.com>
2019-04-01 22:09:44 +0000

    Day 2 Project

--- js/script.js
+++ js/script.js
@@ -4,8 +4,18 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
-});
+    var randomIndex= Math.floor(Math.random() * 25);
+    $('.result').empty();
+      var userinput = $("#search-term").val();
+      var request_url = "https://api.giphy.com/v1/gifs/search?q=" + userinput + "&rating=pg&api_key=dc6zaTOxFJmzC";
 
+  $.ajax({
+      url: request_url,
+      method: "GET",
+        success: function(response) {
+            var pic = response.data[randomIndex].images.original.url;
+            $('.result').append('<img src=' + pic + '>');
+        }
+   
+  })
+});
